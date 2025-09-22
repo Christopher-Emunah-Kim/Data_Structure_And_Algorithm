00504605d9bf65e7ee7f571951f5e4de9bc3e5aa
Christopher-Emunah-Kim <hashin.chris@gmail.com>
2025-09-22 14:21:09 +0900

    Folder Reorganization

--- Data_Structure_Algorithm.cpp
+++ Data_Structure_Algorithm.cpp
@@ -1,66 +1,8 @@
 ﻿
 #include <iostream>
-#include "DataStructure/Container_Adaptor/MyQueue.h"
 
 int main()
 {
-	//테스트용 메뉴 만들기
-	//1. Enqueue
-	//2. Dequeue
-	//3. Peek
-	//4. TestPrintQueue
-	//5. 종료
-	MyQueue q;
 	
-	while (true)
-	{
-		printf("큐 테스트 프로그램\n");
-		printf("1. Enqueue\n2. Dequeue\n3. Peek\n4. TestPrintQueue\n5. 종료\n");
-
-		int choice;
-		std::cin >> choice;
-
-		switch (choice)
-		{
-		case 1:
-		{
-			int value;
-			std::cout << "Enqueue할 값 입력: ";
-			std::cin >> value;
-			q.Enqueue(value);
-			q.TestPrintQueue();
-			break;
-		}
-		case 2:
-		{
-			q.Dequeue();
-			q.TestPrintQueue();
-			break;
-		}
-		case 3:
-		{
-			try
-			{
-				int frontValue = q.Peek();
-				std::cout << "Peek한 값: " << frontValue << std::endl;
-			}
-			catch (const std::out_of_range& e)
-			{
-				std::cout << e.what() << std::endl;
-			}
-		}
-		case 4:
-		{
-			q.TestPrintQueue();
-			break;
-		}
-		case 5:
-		{
-			std::cout << "프로그램 종료" << std::endl;
-			return 0;
-		}
-		}
-	}
-
 
 }
